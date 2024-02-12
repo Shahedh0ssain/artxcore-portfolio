5a225edb950ef8326ed65143f9f5d24c005448b6
Your Name <mdshahed0hossain@gmail.com>
2024-02-12 09:16:44 -0800

    ddd

--- app/loading.js
+++ app/loading.js
@@ -1,4 +1,8 @@
 export default function Loading() {
+    
     // You can add any UI inside Loading, including a Skeleton.
-    return <p>Loading...</p>
+
+    return <div className="h-screen flex justify-center">
+        <span className="loading loading-dots loading-lg"></span>
+    </div>
 }
